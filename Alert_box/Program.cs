fc0e3b930ded0de1530f1dcfedf42e732d01cc22
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2021-01-22 20:45:09 +0530

    Add project files.

---
+++ Alert_box/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        string url = "https://testing.todorvachev.com/special-elements/alert-box/";
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl(url);
+
+        IAlert alert = driver.SwitchTo().Alert();
+
+        System.Console.WriteLine(alert.Text);
+        alert.Accept();
+
+        IWebElement element = driver.FindElement(By.CssSelector("#post-119 > div > figure > img"));
+        if(element.Displayed)
+        {
+            System.Console.WriteLine("Alert was successfully accepted");
+        }
+        else
+        {
+            System.Console.WriteLine("Alert was NOT accepted");
+        }
+
+        Thread.Sleep(3000);
+
+        driver.Quit();
+    }
+}
+
